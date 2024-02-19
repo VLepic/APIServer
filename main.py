523036599fe6c499c9044482759f583f61c16a2d
VLepic <lepic.vaclav@gmail.com>
2024-02-19 23:33:19 +0100

    final

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask_cors import CORS
 
 from approutes.absolute_pressure import absolute_pressure_route
+# Importing the routes
+from approutes.all_weather import all_weather_route
 from approutes.daily_rain import daily_rain_route
 from approutes.dewpoint import dewpoint_route
 from approutes.hourly_rain import hourly_rain_route
@@ -12,13 +14,11 @@ from approutes.wind_gust import wind_gust_route
 from approutes.wind_speed import wind_speed_route
 from approutes.yearly_rain import yearly_rain_route
 
-# Importing the routes
-
 app = Flask(__name__)
 CORS(app)
 
 # Data routes
-
+all_weather_route(app)
 outdoor_temperature_route(app)
 dewpoint_route(app)
 wind_speed_route(app)
